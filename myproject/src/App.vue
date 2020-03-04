abaacacf6140bf8af001b556767eaba0cb477301
LawsonUK <chrislawsonuk@hotmail.com>
2020-03-04 17:12:07 +0000

    adding friend components

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -1,15 +1,45 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
+    <Navbar />
+    <AllFriends :friends="friends" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
+import Navbar from "./Navbar";
+import AllFriends from "./AllFriends";
+import OnlineFriends from "./OnlineFriends";
+
 export default {
   name: "app",
+  components: {
+    Navbar,
+    OnlineFriends,
+    AllFriends
+  },
   data() {
     return {
-      title: "My first Vue app wooo :-)"
+      title: "My first Vue app wooo :-)",
+      friends: [
+        {
+          name: "Mario",
+          online: true
+        },
+        {
+          name: "Luigi",
+          online: false
+        },
+        {
+          name: "Toad",
+          online: true
+        },
+        {
+          name: "Bowser",
+          online: false
+        }
+      ]
     };
   }
 };
