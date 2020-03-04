ad0bf96edd75e99c653a764aa754236232c11fd2
Christopher Lawson <c.lawson@barclaycomms.com>
2020-03-04 13:35:49 +0000

    completed section 2

--- app.js
+++ app.js
@@ -2,11 +2,41 @@ new Vue({
     el: '#app',
     data: {
         title: "Becoming a Vue Ninja",
-        wage: 10
+        name: 'Ryu',
+        coords: {
+            x: 0,
+            y: 0
+        },
+        showName: true,
+        showAge: true,
+        items: ['Mushrooms', 'Green Shells', 'Red Shells', 'Banana'],
+        ninjas: [
+            {
+                name: 'Crystal',
+                age: 25,
+                belt: 'Black'
+            },
+            {
+                name: 'Ryu',
+                age: 30,
+                belt: 'Brown'
+            },
+            {
+                name: 'Ken',
+                age: 35,
+                belt: 'Orange'
+            }
+        ]
     },
     methods: {
-        changeWage(amount) {
-            this.wage += amount
+        logMessage() {
+            console.log('Hello World')
+        },
+        toggleName() {
+            this.showName = !this.showName
+        },
+        toggleAge() {
+            this.showAge = !this.showAge
         }
     }
 })
